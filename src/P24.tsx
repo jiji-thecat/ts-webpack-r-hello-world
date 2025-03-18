61863133972a77ab90089b97f6b62a06839e9671
jiji-thecat <jijithekawaiicat@gmail.com>
2025-03-18 17:34:41 +0000

    .

--- src/P24.tsx
+++ src/P24.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
 
 const employee = [
   { id: 0, name: 'Yamada', position: 'Engineer', salary: 1000 },
@@ -6,40 +6,143 @@ const employee = [
   { id: 2, name: 'Tanaka', position: 'CTO', salary: 3000 },
 ];
 
-const AddComponent = () => {
+const AddComponent = ({
+  onUpdateData,
+}: {
+  onUpdateData: ({ name, position, salary }: { name: string; position: string; salary: string }) => void;
+}) => {
+  const [name, setName] = useState('');
+  const [position, setPosition] = useState('');
+  const [salary, setSalary] = useState('');
+
+  const isSaveDisabled = useMemo(() => name === '' || position === '' || salary === '', [name, position, salary]);
+
+  const onChangeName = (e: any) => {
+    setName(e.target.value);
+  };
+
+  const onChangePosition = (e: any) => {
+    setPosition(e.target.value);
+  };
+
+  const onChangeSalary = (e: any) => {
+    setSalary(e.target.value);
+  };
+
+  const onClickSave = () => {
+    onUpdateData({ name, position, salary });
+    setName('');
+    setPosition('');
+    setSalary('');
+  };
+
   return (
     <tr>
       <td>
-        <input type="text"></input>
+        <input type="text" value={name} onChange={onChangeName}></input>
       </td>
       <td>
-        <input type="text"></input>
+        <input type="text" value={position} onChange={onChangePosition}></input>
       </td>
       <td>
-        <input type="number"></input>
+        <input type="number" value={salary} onChange={onChangeSalary}></input>
       </td>
-      <button>Save</button>
+      <td>
+        <button disabled={isSaveDisabled} onClick={onClickSave}>
+          Save
+        </button>
+      </td>
     </tr>
   );
 };
 
-const RowComponent = ({ name, position, salary }: { name: string; position: string; salary: number }) => {
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{position}</td>
-      <td>
-        {salary}
-        <input type="number"></input>
-      </td>
+/**
+ * 子では、一部を更新するだけ、なので、見た目のstateとかそういうのを管理する。
+ * データその物を更新する場合は親の関数を呼び出す。
+ */
 
-      <button>Save</button>
-    </tr>
+const RowComponent = ({
+  name,
+  id,
+  position,
+  salary,
+  updateSalary,
+}: {
+  name: string;
+  id: number;
+  position: string;
+  salary: number;
+  updateSalary: (id: number, salary: number) => void;
+}) => {
+  const [isInputVisible, setIsInputVisible] = useState(false);
+  const [isSaveDisabled, setIsSaveDisabled] = useState(true);
+  const [salaryInput, setSalaryInput] = useState(salary);
+
+  const onClickSalary = () => {
+    setIsInputVisible(true);
+  };
+
+  const onClickSave = () => {
+    updateSalary(id, salaryInput);
+    setIsInputVisible(false);
+    setIsSaveDisabled(true);
+  };
+
+  const onChangeSalaryInput = (e: any) => {
+    const newSalary = e.target.value;
+    setSalaryInput(newSalary);
+
+    if (newSalary && parseInt(newSalary) !== salary) {
+      setIsSaveDisabled(false);
+    } else {
+      setIsSaveDisabled(true);
+    }
+  };
+
+  return (
+    <>
+      <tr>
+        <td>{name}</td>
+        <td>{position}</td>
+        <td onClick={onClickSalary}>
+          {!isInputVisible && salary}
+          {isInputVisible && <input type="number" value={salaryInput} onChange={onChangeSalaryInput}></input>}
+        </td>
+        <td>
+          <button disabled={isSaveDisabled} onClick={onClickSave}>
+            Save
+          </button>
+        </td>
+      </tr>
+    </>
   );
 };
 
+interface dataObject {
+  id: number;
+  name: string;
+  position: string;
+  salary: number;
+}
+
+/**
+ * idを使ってデータを更新する。親で。
+ */
 export default () => {
-  const [data, setData] = useState(employee);
+  const [data, setData] = useState<dataObject[]>(employee);
+
+  const updateSalary = (id: number, salary: number) => {
+    const newData = [...data];
+    const current = newData.find((v) => v.id === id);
+    current!.salary = salary;
+
+    setData(newData);
+  };
+
+  const onUpdateData = ({ name, position, salary }: { name: string; position: string; salary: string }) => {
+    const newData = [...data, { id: data.length, name, position, salary: parseInt(salary) }];
+    setData(newData);
+  };
 
   return (
     <>
@@ -54,9 +157,16 @@ export default () => {
           </thead>
           <tbody>
             {data.map((v, id) => (
-              <RowComponent key={id} name={v.name} position={v.position} salary={v.salary} />
+              <RowComponent
+                key={id}
+                id={v.id}
+                name={v.name}
+                position={v.position}
+                salary={v.salary}
+                updateSalary={updateSalary}
+              />
             ))}
-            <AddComponent />
+            <AddComponent onUpdateData={onUpdateData} />
           </tbody>
         </table>
       </div>
